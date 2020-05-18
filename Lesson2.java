df9d85213a7ec102ed26ea7ad7ea0e3196c517f4
Anastasia Shpakova <53617426+nastachka2019@users.noreply.github.com>
2020-05-18 13:37:46 +0300

    Update and rename Lesson2.txt to Lesson2.java

--- Lesson2.java
+++ Lesson2.java
@@ -3,7 +3,7 @@ package com.company;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
+public class Lesson2 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // для ввода размера массива
