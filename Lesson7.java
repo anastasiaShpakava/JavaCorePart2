ae11aff5c7e3f3fa2be038e4183d5aad5a6a62cb
Anastasia Shpakova <53617426+nastachka2019@users.noreply.github.com>
2020-05-18 13:35:45 +0300

    Update and rename Lesson7.txt to Lesson7.java

--- Lesson7.java
+++ Lesson7.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class Main {
+public class Lesson7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter your word");
